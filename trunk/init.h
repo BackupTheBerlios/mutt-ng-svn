229948d1b3fbb16a36f0df54c5c63b984e936b5c
pdmef <pdmef@e385b8ad-14ed-0310-8656-cc95a2468c6d>
2005-03-10 15:53:04 +0000

    Rocco Rutte: included CD's ifdef patch

--- trunk/init.h
+++ trunk/init.h
@@ -3525,6 +3525,7 @@ static int parse_lists (BUFFER *, BUFFER *, unsigned long, BUFFER *);
 static int parse_unlists (BUFFER *, BUFFER *, unsigned long, BUFFER *);
 static int parse_alias (BUFFER *, BUFFER *, unsigned long, BUFFER *);
 static int parse_unalias (BUFFER *, BUFFER *, unsigned long, BUFFER *);
+static int parse_ifdef (BUFFER *, BUFFER *, unsigned long, BUFFER *);
 static int parse_ignore (BUFFER *, BUFFER *, unsigned long, BUFFER *);
 static int parse_unignore (BUFFER *, BUFFER *, unsigned long, BUFFER *);
 static int parse_source (BUFFER *, BUFFER *, unsigned long, BUFFER *);
@@ -3571,6 +3572,7 @@ struct command_t Commands[] = {
 #endif
   { "hdr_order",	parse_list,		UL &HeaderOrderList },
 #ifdef HAVE_ICONV
+  { "ifdef",		parse_ifdef,		0 },
   { "iconv-hook",	mutt_parse_hook,	M_ICONVHOOK }, 
 #endif
   { "ignore",		parse_ignore,		0 },
