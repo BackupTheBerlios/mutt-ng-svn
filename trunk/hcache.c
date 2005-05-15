7131fff423bb3d4377e774aec2d61487ae52920a
pdmef <pdmef@e385b8ad-14ed-0310-8656-cc95a2468c6d>
2005-05-15 14:01:56 +0000

    Rocco Rutte: qdbm vlopen improvement (patch again by Thomas Glanzmann)

--- trunk/hcache.c
+++ trunk/hcache.c
@@ -617,7 +617,7 @@ void *
 mutt_hcache_open(const char *path, const char *folder)
 {
   struct header_cache *h = safe_calloc(1, sizeof (HEADER_CACHE));
-  int    flags = 0;
+  int    flags = VL_OWRITER | VL_OCREAT;
 #if 0 /* FIXME */
   int pagesize = atoi(HeaderCachePageSize) ? atoi(HeaderCachePageSize) : 16384;
 #endif
@@ -635,14 +635,9 @@ mutt_hcache_open(const char *path, const char *folder)
   path = mutt_hcache_per_folder(path, folder);
 
   if (option(OPTHCACHECOMPRESS))
-    flags = VL_OZCOMP;
+    flags |= VL_OZCOMP;
 
-  h->db = vlopen(path, flags | VL_OWRITER | VL_OCREAT, VL_CMPLEX);
-  if (h->db)
-    return h;
-
-  /* if rw failed try ro */
-  h->db = vlopen(path, flags | VL_OREADER, VL_CMPLEX);
+  h->db = vlopen(path, flags, VL_CMPLEX);
   if (h->db)
     return h;
   else
