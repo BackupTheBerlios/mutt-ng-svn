04991b014095997fb625d629dde7fbbd6aad9314
pdmef <pdmef@e385b8ad-14ed-0310-8656-cc95a2468c6d>
2005-07-27 16:19:09 +0000

    Rocco Rutte: - merge in latest mutt changes

--- trunk/crypt-gpgme.c
+++ trunk/crypt-gpgme.c
@@ -600,7 +600,7 @@ static int set_signer (gpgme_ctx_t ctx, int for_smime)
     gpgme_key_release (key);
     gpgme_key_release (key2);
     gpgme_release (listctx);
-    mutt_error (_("ambiguous specfication of secret key `%s'\n"), signid);
+    mutt_error (_("ambiguous specification of secret key `%s'\n"), signid);
     return -1;
   }
   gpgme_op_keylist_end (listctx);
@@ -988,7 +988,7 @@ static int show_sig_summary (unsigned long sum,
     gpgme_signature_t sig;
     unsigned int i;
 
-    state_attach_puts (_("A system error occured"), s);
+    state_attach_puts (_("A system error occurred"), s);
 
     /* Try to figure out some more detailed system error information. */
     result = gpgme_op_verify_result (ctx);
