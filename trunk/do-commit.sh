c03ab8712c6a46a4e690b67bdfdbf9e6e5474296
pdmef <pdmef@e385b8ad-14ed-0310-8656-cc95a2468c6d>
2005-08-25 18:37:36 +0000

    Rocco Rutte: - fixup version.h generation to always use VERSION.svn (renamed from VERSION.data) via make(1) dependency (hopefully _this_ was the last time I had to deal with it)

--- trunk/do-commit.sh
+++ trunk/do-commit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 svn update
 CURREV=`svn info | grep '^Revision' | cut -d : -f 2`
-echo "`echo $CURREV+1 | bc`" > ./VERSION.date
+echo "`echo $CURREV+1 | bc`" > ./VERSION.svn
 svn commit "$@"
