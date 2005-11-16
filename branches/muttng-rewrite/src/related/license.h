b0214f2c877dc3ec620a8fccc99f32b0f90d0add
pdmef <pdmef@e385b8ad-14ed-0310-8656-cc95a2468c6d>
2005-11-16 12:16:25 +0000

    Rocco Rutte: - rewrite: add some "Related Pages" for doxygen with license and (very few) coding style rules

---
+++ branches/muttng-rewrite/src/related/license.h
@@ -0,0 +1,8 @@
+/**
+ * @page page_license License: GNU General Public License
+ *
+ *   The mutt-ng source code is available subject to the GNU General
+ *   Pulic License, version 2.
+ *
+ *   @verbinclude GPL
+ */
---
+++ branches/muttng-rewrite/src/related/mainpage.h
@@ -0,0 +1,12 @@
+/**
+ * @mainpage
+ *
+ * This is the documentation for the in-progress rewrite of mutt-ng.
+ *
+ * Lots of documentation is still missing except:
+ *
+ *   - if you want to help rewriting it, please make sure to have at
+ *     look at @ref page_coding.
+ *   - the license agreement: @ref page_license
+ *
+ */
