44282ca2eeafe0cc399d94e415146e2027993803
ak1 <ak1@e385b8ad-14ed-0310-8656-cc95a2468c6d>
2005-12-05 09:41:17 +0000

    Andreas Krennmair:   - rewrite: add Header implementation.

--- branches/muttng-rewrite/src/libmuttng/message/header.cpp
+++ branches/muttng-rewrite/src/libmuttng/message/header.cpp
@@ -5,6 +5,51 @@
  * @brief MIME message header
  */
 #include "header.h"
+#include <cstring>
 
-Header::Header (void) {}
-Header::~Header (void) {}
+Header::Header (char * name, char * body) {
+  buffer_init(&this->name);
+  buffer_init(&this->body);
+
+  buffer_add_str(&this->name,name,-1);
+  buffer_add_str(&this->body,body,-1);
+}
+
+Header::Header (buffer_t * name, buffer_t * body) {
+  buffer_init(&this->name);
+  buffer_init(&this->body);
+
+  buffer_add_buffer(&this->name,name);
+  buffer_add_buffer(&this->body,body);
+}
+
+bool Header::equalsName(char * name) {
+  return 0==strcasecmp(name,this->name.str);
+}
+
+bool Header::equalsName(buffer_t * name) {
+  return equalsName(name->str);
+}
+
+buffer_t * Header::getName() {
+  return &name;
+}
+
+buffer_t * Header::getBody() {
+  return &body;
+}
+
+void Header::setName(buffer_t * name) {
+  buffer_shrink(&this->name,0);
+  buffer_add_buffer(&this->name,name);
+}
+
+void Header::setBody(buffer_t * body) {
+  buffer_shrink(&this->body,0);
+  buffer_add_buffer(&this->body,body);
+}
+
+Header::~Header(void) {
+  buffer_free(&name);
+  buffer_free(&body);
+}
--- branches/muttng-rewrite/src/libmuttng/message/header.h
+++ branches/muttng-rewrite/src/libmuttng/message/header.h
@@ -2,18 +2,80 @@
 /**
  * @file libmuttng/message/header.h
  * @author Rocco Rutte <pdmef@cs.tu-berlin.de>
+ * @author Andreas Krennmair <ak@synflood.at>
  * @brief MIME message header
  */
 #ifndef LIBMUTTNG_MESSGAGE_HEADER_H
 #define LIBMUTTNG_MESSGAGE_HEADER_H
 
 #include "../libmuttng.h"
+#include "core/buffer.h"
 
-/** Message header class */
+/**
+ * Message header class. Implements an internet message header
+ * according to RFC 2822.
+ */
 class Header : public LibMuttng {
   public:
-    Header (void);
+    /**
+    * Constructor.
+    * @param name header name as C string.
+    * @param body header body as C string.
+    */
+    Header(char * name = "", char * body = "");
+
+    /**
+    * Constructor.
+    * @param name header name as @c buffer_t pointer.
+    * @param body header body as @c buffer_t pointer.
+    */
+    Header(buffer_t * name, buffer_t * body);
+
+    /**
+     * Destructor.
+     */
     ~Header (void);
+
+    /**
+     * Compares the header name with a string in a case-insensitive way.
+     * @param name header name to compare with.
+     * @return true if name equals the header name, otherwise false.
+     */
+    bool equalsName(char * name);
+
+    /**
+     * Compares the header name with a buffer_t in a case-insensitive way.
+     * @param name header name to compare with.
+     * @return true if name equals the header name, otherwise false.
+     */
+    bool equalsName(buffer_t * name);
+
+    /**
+     * Getter function for header name.
+     * @return header name as @c buffer_t pointer.
+     */
+    buffer_t * getName();
+
+    /**
+     * Getter function for header body.
+     * @return header body as @c buffer_t pointer.
+     */
+    buffer_t * getBody();
+
+    /**
+     * Setter function for header name.
+     * @param name new header name.
+     */
+    void setName(buffer_t * name);
+
+    /**
+     * Setter function for header body.
+     * @param body new header body.
+     */
+    void setBody(buffer_t * body);
+  private:
+    buffer_t name;
+    buffer_t body;
 };
 
 #endif /* !LIBMUTTNG_MESSGAGE_HEADER_H */
