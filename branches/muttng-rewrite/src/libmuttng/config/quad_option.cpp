cbf4047e93cec9b44ed8b83d773e0e657b16d4ee
pdmef <pdmef@e385b8ad-14ed-0310-8656-cc95a2468c6d>
2005-12-13 14:21:44 +0000

    Rocco Rutte: - rewrite: oops, add missing quad-option files

---
+++ branches/muttng-rewrite/src/libmuttng/config/quad_option.cpp
@@ -0,0 +1,70 @@
+/** @addtogroup libmuttng_config */
+/**
+ * @file libmuttng/config/quad_option.cpp
+ * @author Rocco Rutte <pdmef@cs.tu-berlin.de>
+ * @brief Implementation: Quad option
+ */
+#include "quad_option.h"
+
+#include "core/intl.h"
+#include "core/str.h"
+
+QuadOption::QuadOption(const char* name_, const char* init_,
+                       unsigned short* store_) : Option(name_,init_), store(store_) {
+  set(init_,NULL);
+}
+
+bool QuadOption::set(const char* value, buffer_t* error) {
+  if (!value)
+    return unset();
+
+  unsigned short old = *store;
+  if (str_eq(value,"yes"))
+    *store = M_YES;
+  else if (str_eq(value,"no"))
+    *store = M_NO;
+  else if (str_eq(value,"ask-yes"))
+    *store = M_ASK_YES;
+  else if (str_eq(value,"ask-no"))
+    *store = M_ASK_NO;
+  else {
+    if (error) {
+      buffer_add_ch(error,'\'');
+      buffer_add_str(error,value,-1);
+      buffer_add_str(error,_("' is invalid for $"),-1);
+      buffer_add_str(error,name,-1);
+    }
+    return false;
+  }
+  if (old != *store)
+    sigOptionChange.emit(this);
+  return true;
+}
+
+bool QuadOption::unset() { *store = M_NO; return true; }
+bool QuadOption::reset() { set(init,NULL); return true; }
+
+bool QuadOption::toggle() {
+  switch ((QuadOption::state)*store) {
+  case M_NO: *store = M_YES; break;
+  case M_YES: *store = M_NO; break;
+  case M_ASK_YES: *store = M_ASK_NO; break;
+  case M_ASK_NO: *store = M_ASK_YES; break;
+  }
+  return true;
+}
+
+bool QuadOption::query(buffer_t* dst) {
+  if (!dst) return false;
+  switch ((QuadOption::state)*store) {
+  case M_NO: buffer_add_str(dst,"yes",3); break;
+  case M_YES: buffer_add_str(dst,"no",2); break;
+  case M_ASK_YES: buffer_add_str(dst,"ask-yes",7); break;
+  case M_ASK_NO: buffer_add_str(dst,"ask-no",6); break;
+  }
+  return true;
+}
+
+const char* QuadOption::getType() { return _("quad-option"); }
+
+bool QuadOption::validity(buffer_t* dst) { (void) dst; return false; }
---
+++ branches/muttng-rewrite/src/libmuttng/config/quad_option.h
@@ -0,0 +1,37 @@
+/** @addtogroup libmuttng_config */
+/**
+ * @file libmuttng/config/quad_option.h
+ * @author Rocco Rutte <pdmef@cs.tu-berlin.de>
+ * @brief Boolerface: Quad option
+ */
+#ifndef LIBMUTTNG_CONFIG_QUAD_OPTION_H
+#define LIBMUTTNG_CONFIG_QUAD_OPTION_H
+
+#include "option.h"
+
+/**
+ * Quad option class
+ */
+class QuadOption : public Option {
+  public:
+    /**
+     * Create new boolean option.
+     * @param name_ Name.
+     * @param init_ Initial value.
+     * @param store_ Storage for value.
+     */
+    QuadOption(const char* name_, const char* init_, unsigned short* store_);
+    bool set(const char* value, buffer_t* error);
+    bool unset();
+    bool reset();
+    bool toggle();
+    bool query(buffer_t* dst);
+    const char* getType();
+    bool validity(buffer_t* dst);
+  private:
+    /** quad-option states */
+    enum state { M_YES = 0, M_NO, M_ASK_YES, M_ASK_NO };
+    unsigned short* store;
+};
+
+#endif
